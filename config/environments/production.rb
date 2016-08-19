62523f18fe4fce1f6e478bc253fbed75aafc1e75
Dicky Ho <me@ckho.info>
2016-08-19 18:17:16 +0800

    Config: Mongo - Allow Inheritance in Models

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,6 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
+
+  config.mongoid.preload_models = true
 end
