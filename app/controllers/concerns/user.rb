a3a074c2aa2c83d51657296a42d36d17367df5c5
Dicky Ho <me@ckho.info>
2016-08-24 18:03:43 +0800

    #36

---
+++ app/controllers/concerns/user.rb
@@ -0,0 +1,11 @@
+module Bank
+  extend ActiveSupport::Concern
+
+  def activate_member(operator, member, cucardid)
+    member.cu_identity_no = cucardid
+    member.activated = true
+    member.activated_at = Time.now
+    member.save
+  end
+
+end
