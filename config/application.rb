29a37753a47de2fc84e4426aef36f0e7c7f502bb
Dicky Ho <me@ckho.info>
2016-09-01 11:39:58 +0800

    Allow Classifying Translation

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,8 @@ module CUHappyCorner
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :en
   end
 end
