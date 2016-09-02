3b856d14f85b2cf7f8d655bb3a1cae4e9db0d9dc
Dicky Ho <me@ckho.info>
2016-09-03 01:35:45 +0800

    Seed Data: Matrix

---
+++ db/seeds/matrix.seeds.rb
@@ -0,0 +1,8 @@
+require File.expand_path("../../../config/environment", __FILE__)
+
+matrix = Computer.new
+I18n.locale = :en
+matrix.name = "The Matrix" 
+I18n.locale = :zh_HK
+matrix.name = "母體"
+f0.save
