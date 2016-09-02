496854510183f87902f60e12d65559e00e2f93ae
Dicky Ho <me@ckho.info>
2016-09-03 01:33:58 +0800

    Member System: Activation Module

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'corner/users/activations' => 'corner/users/activations#index'
+  post 'corner/users/activations' => 'corner/users/activations#show'
+  put 'corner/users/activations' => 'corner/users/activations#update'
 end
