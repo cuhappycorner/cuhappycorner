fd29c2a0a0a0af2fe9f20910112d845ca0838d8b
Dicky Ho <me@ckho.info>
2016-09-12 01:53:51 +0800

    Update User Activation

---
+++ app/controllers/concerns/member_concern.rb
@@ -0,0 +1,30 @@
+module MemberConcern
+  extend ActiveSupport::Concern
+
+  def activate_member(operator, member, cu_link_id)
+    member.cu_link_id = cu_link_id
+    member.activated = true
+    member.activated_at = Time.zone.now
+    if not member.save
+      flash[:error] << member.errors.full_messages
+      return false
+    else
+      return true
+    end
+  end
+
+  # TO DO!
+  def mirgrate_member(member, amount)
+    operator = Computer.first
+    project = Corner::Account::Project.find_by(name: "2016 Sep System Migration")
+
+    transaction = Corner::Account::MigrationCreditTransaction.create(operator: operator, debitor: HappyCorner.first.account.first, creditor: member.account, detail: "Migration from Old System 舊系統遷移", project: project)
+    if not transaction.valid?
+      flash[:error] << transaction.errors.full_messages
+      return false
+    else
+      return true
+    end
+  end
+
+end
--- app/controllers/corner/users/activations_controller.rb
+++ app/controllers/corner/users/activations_controller.rb
@@ -1,30 +1,42 @@
 class Corner::Users::ActivationsController < ApplicationController
-  include Member
-  include Bank
+  include MemberConcern
+  include Bank::AccountConcern
 
   before_action :authenticate_user!
 
   # GET
   def index
-
+    if !current_user.role.include? Role.find_by(name:"shopkeeper")
+      flash[:alert] = t('error.notauthorized')
+      redirect_to(request.referrer || root_path) and return
+    end
   end
 
   # GET
   def show
+    if !current_user.role.include? Role.find_by(name:"shopkeeper")
+      flash[:alert] = t('error.notauthorized')
+      redirect_to(request.referrer || root_path) and return
+    end
     @user = User.find_by(cuid: params[:cuid])
     if @user == nil
       flash[:notice] = t('corner.users.activations.show.error.notfound')
-      redirect_to action:'index'
+      redirect_to action:'index' and return
     end
   end
 
   # PUT
   def update
+    if !current_user.role.include? Role.find_by(name:"shopkeeper")
+      flash[:alert] = t('error.notauthorized')
+      redirect_to(request.referrer || root_path) and return
+    end
     @user = User.find_by(cuid: params[:cuid])
     if @user == nil
       flash[:notice] = t('corner.users.activations.show.error.notfound')
-      redirect_to action:'index'
+      redirect_to action:'index' and return
     end
+
     @user.name_translations[:en] = params[:eng_name]
     @user.name_translations[:zh_HK] = params[:chi_name]
     @user.display_name = params[:display_name]
@@ -35,12 +47,23 @@ class Corner::Users::ActivationsController < ApplicationController
     @user.year_of_admission = params[:year_of_admission]
     @user.year_of_graduation = params[:year_of_graduation]
     @user.save
+    if not @user.valid?
+      flash[:error] = @user.errors.full_messages
+      redirect_to(request.referrer || root_path) and return
+    end
 
-    activate_member(current_user, @user, params[:cu_link_id])
-    bank_create_individual_account(current_user, @user)
+    member = activate_member(current_user, @user, params[:cu_link_id])
+    if not member
+      redirect_to(request.referrer || root_path) and return
+    end
+
+    status = bank_create_individual_account(current_user, @user)
+    if not status
+      redirect_to(request.referrer || root_path) and return
+    end
 
     flash[:success] = t('corner.users.activations.show.success.activated')
-    redirect_to action:'index'
+    redirect_to action:'index' and return
 
   end
 end
