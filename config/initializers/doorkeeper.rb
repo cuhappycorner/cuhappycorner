d0a03ccb73b1ef3fd6b8e474d7137e4ecf24d930
Dicky Ho <me@ckho.info>
2017-04-22 00:50:22 +0800

    Doorkeeper Setting Updated

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -61,7 +61,7 @@ Doorkeeper.configure do
   # Define access token scopes for your provider
   # For more information go to
   # https://github.com/doorkeeper-gem/doorkeeper/wiki/Using-Scopes
-  default_scopes  :public
+  default_scopes  :user_info
   optional_scopes :write, :update
 
   # Change the way client credentials are retrieved from the request object.
