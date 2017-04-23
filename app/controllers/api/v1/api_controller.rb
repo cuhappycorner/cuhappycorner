34ca27673f9230b4bd262a90911412b18b0eee14
Dicky Ho <me@ckho.info>
2017-04-23 13:12:18 +0800

    Allow Flexible Authentication for API

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,5 +1,6 @@
 module Api::V1
   class ApiController < ::ApplicationController
+    skip_before_action :verify_authenticity_token
     private
     def current_resource_owner
       User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
