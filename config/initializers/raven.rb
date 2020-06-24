d93c91d6a82bdad69c035443f3c2abd4361efaf1
Chunkit Ho <me@ckho.info>
2020-06-25 01:13:46 +0800

    Update raven.rb

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,6 +1,6 @@
 require 'raven'
 
 Raven.configure do |config|
-  config.dsn = 'https://5803a61671ba4619824cd55db1a97ec5:fdb7449ac5ec453dae92e1c212940c3f@sentry.io/103024'
+  config.dsn = '${RAVEN_KEY}'
   config.environments = %w(staging production)
 end
